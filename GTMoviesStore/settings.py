4acbd74d0e00e64bde6529224279ec779550fc4d
satvikmarripalapu <marripalapusatvik@gmail.com>
2025-02-01 19:56:05 -0500

    Chapter 3 Home/About Pages
    WIP: Header and Footer

--- GTMoviesStore/settings.py
+++ GTMoviesStore/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -55,8 +56,7 @@ ROOT_URLCONF = 'GTMoviesStore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [os.path.join(BASE_DIR,'GTMoviesStore/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'GTMoviesStore/static/',
+]
