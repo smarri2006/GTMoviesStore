763c9f4a672a7213ca37ac185fd8f9ea2fbdc606
jRamirez305 <jramirez305@gatech.edu>
2025-02-08 16:38:53 -0500

    End of Chpt.6

--- movies/views.py
+++ movies/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render
 
 from .models import Movie
 def index(request):
+    search_term = request.GET.get('search')
+    if search_term:
+        movies = Movie.objects.filter(name__icontains=search_term)
+    else:
+        movies = Movie.objects.all()
     template_data = {}
     template_data['title'] = 'Movies'
-    template_data['movies'] = Movie.objects.all()
+    template_data['movies'] = movies
     return render(request, 'movies/index.html',
 {'template_data': template_data})
 
