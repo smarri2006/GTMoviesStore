4a8c4003be267b65148f0a50241b8331d760e6c0
JasonALow <jasonalow8@gmail.com>
2025-02-15 19:09:57 -0500

    Previous again

--- GTMoviesStore/settings.py
+++ GTMoviesStore/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^+o2x1kf0@tx9vl%3%4=qm60=_ba!@$lg(-=$$c03zd(3fmawb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
